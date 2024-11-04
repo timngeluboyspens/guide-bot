f5a771f35eeed8c7b9dfb30d50b2c6d121f5bc2d
khanzafa <khanzafadilaazz@gmail.com>
2024-11-04 23:44:04 +0700

    add timeout worker

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start Flask app
 echo "Starting Flask app..."
-gunicorn --workers 2 --bind 0.0.0.0:5000 --log-level info wsgi:app &
+gunicorn --workers 2 --timeout 120 --bind 0.0.0.0:5000 --log-level info wsgi:app &
 
 # Start Telegram bot
 echo "Starting Telegram bot..."
